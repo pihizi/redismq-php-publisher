bdfbb1993b966d37e7a552c5210cc6da85b4de5f
Hongjie Zhu <hongjie.zhu@geneegroup.com>
2014-11-14 17:12:17 +0800

    init

---
+++ src/Publisher.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RedisMQ;
+
+class Publisher
+{
+    private $_redis;
+    public function __construct($host='127.0.0.1', $port='6379', $password=null)
+    {
+        $redis = new \Redis();
+        $redis->connect($host, $port);
+        if (!empty($password)) {
+            $redis->auth($password);
+        }
+        $this->_redis = $redis;
+    }
+
+    public function __destruct()
+    {
+        $this->_redis->close();
+    }
+
+    public function send($channel, $emssage)
+    {
+        $this->_redis->publish($channel, $message);
+    }
+}
